b535f045ba964f7e6473f932f66b336735f21068
kl-mec <64818175+kl-mec@users.noreply.github.com>
2020-08-31 23:34:59 -0400

    Update count.py

--- count.py
+++ count.py
@@ -1,13 +1,2 @@
-# Write a function that prints the total number of lines in all the files in the `/files` directory.
-# For example, if there are 3 files with 10 lines each, your function should print `30`.
-# Your function should still work even if I add or take away files from the directory.
-
-# Extension #1 (optional): Show additional statistics besides the total number of files, 
-# such as the average number of lines per file, or the average number of words per line.
-
-# Extension #2 (optional): Write another function, `count_unique`, that only adds up the total 
-# number of lines from unique files. For example, if there are three files, A, B, and C, 
-# which each have 10 lines, and A and C are the exact same, then you should ignore A, and print 20.
-
 def count():
   # Write your function here
